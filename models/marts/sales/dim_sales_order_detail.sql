08b69cc0394d86226d32600cd4ffa3d7415005a4
Daniel Magalhães <powrrless@gmail.com>
2024-11-24 02:31:14 +0000

    add surrogate keys to models

--- models/marts/sales/dim_sales_order_detail.sql
+++ models/marts/sales/dim_sales_order_detail.sql
@@ -1,16 +1,37 @@
 with
-    fact as (
-        select
-            sales_order_detail_pk
-            , sales_order_fk
-            , product_fk
-            , carrier_tracking_number
-            , order_quantity
-            , unit_price
-            , unit_price_discount
-            , discount_type
-            , discount_description
+    sales_order_detail_tb as (
+        select *
         from {{ ref('int_sales_order_detail') }}
+    )
+
+    , product_tb as (
+        select
+            product_sk
+            , product_id
+        from {{ ref('dim_product') }}
+    )
+
+    , fact as (
+        select
+            {{
+                dbt_utils.generate_surrogate_key([
+                    'sales_order_detail_tb.sales_order_fk'
+                    , 'product_tb.product_sk'
+                    , 'sales_order_detail_tb.order_quantity'
+                    , 'sales_order_detail_tb.unit_price'
+                ])
+            }} as sales_order_detail_sk
+            , sales_order_detail_tb.sales_order_fk as sales_order_id
+            , product_tb.product_sk as product_fk
+            , sales_order_detail_tb.carrier_tracking_number
+            , sales_order_detail_tb.order_quantity
+            , sales_order_detail_tb.unit_price
+            , sales_order_detail_tb.unit_price_discount
+            , sales_order_detail_tb.discount_type
+            , sales_order_detail_tb.discount_description
+        from sales_order_detail_tb
+        left join product_tb
+            on sales_order_detail_tb.product_fk = product_tb.product_id
     )
 
 select *
--- models/marts/sales/fct_sales_order.sql
+++ models/marts/sales/fct_sales_order.sql
@@ -1,30 +1,53 @@
 with
-    fact as (
-        select
-            sales_order_pk
-            , customer_fk
-            , salesperson_fk
-            , territory_fk
-            , bill_to_address_fk
-            , ship_to_address_fk
-            , ship_method_fk
-            , currency_rate_fk
-            , revision_number
-            , order_date
-            , due_date
-            , ship_date
-            , order_status
-            , online_order_flag
-            , purchase_order_number
-            , account_number
-            , credit_card_approval_code
-            , subtotal
-            , tax_amount
-            , freight
-            , total_due
-            , comment
-            , card_type
+    sales_order_header_tb as (
+        select *
         from {{ ref('int_sales_order_header') }}
+    )
+
+    , customer_tb as (
+        select
+            customer_sk
+            , customer_id
+        from {{ ref('dim_customer') }}
+    )
+
+    , fact as (
+        select
+            {{
+                dbt_utils.generate_surrogate_key([
+                    'sales_order_header_tb.sales_order_pk'
+                    , 'customer_tb.customer_sk'
+                    , 'sales_order_header_tb.ship_to_address_fk'
+                    , 'sales_order_header_tb.order_date'
+                    , 'sales_order_header_tb.subtotal'
+                ])
+            }} as sales_order_sk
+            , sales_order_header_tb.sales_order_pk as sales_order_id
+            , customer_tb.customer_sk as customer_fk
+            , sales_order_header_tb.salesperson_fk as salesperson_id
+            , sales_order_header_tb.territory_fk as territory_id
+            , sales_order_header_tb.bill_to_address_fk as bill_to_address_id
+            , sales_order_header_tb.ship_to_address_fk as ship_to_address_id
+            , sales_order_header_tb.ship_method_fk as ship_method_id
+            , sales_order_header_tb.currency_rate_fk as currency_rate_id
+            , sales_order_header_tb.revision_number
+            , sales_order_header_tb.order_date
+            , sales_order_header_tb.due_date
+            , sales_order_header_tb.ship_date
+            , sales_order_header_tb.order_status
+            , sales_order_header_tb.online_order_flag
+            , sales_order_header_tb.purchase_order_number
+            , sales_order_header_tb.account_number
+            , sales_order_header_tb.credit_card_approval_code
+            , sales_order_header_tb.subtotal
+            , sales_order_header_tb.tax_amount
+            , sales_order_header_tb.freight
+            , sales_order_header_tb.total_due
+            , sales_order_header_tb.comment
+            , sales_order_header_tb.card_type
+        from sales_order_header_tb
+        left join customer_tb
+            on sales_order_header_tb.customer_fk = customer_tb.customer_id
     )
 
 select *
